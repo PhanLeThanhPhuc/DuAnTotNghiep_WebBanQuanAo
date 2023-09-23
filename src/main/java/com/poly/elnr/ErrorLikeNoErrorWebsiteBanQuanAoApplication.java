e57bfda81a44de9b2974fe724b7b0ef51959ad76
PhanLeThanhPhuc <pltp2010@gmail.com>
2023-09-23 15:41:01 +0700

    push code duantotnghiep

---
+++ src/main/java/com/poly/elnr/ErrorLikeNoErrorWebsiteBanQuanAoApplication.java
@@ -0,0 +1,13 @@
+package com.poly.elnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorLikeNoErrorWebsiteBanQuanAoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorLikeNoErrorWebsiteBanQuanAoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/elnr/ErrorLikeNoErrorWebsiteBanQuanAoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.elnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorLikeNoErrorWebsiteBanQuanAoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
