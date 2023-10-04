0a6c199f417a43815e31182df2d360477b8a0b35
PhanLeThanhPhuc <pltp2010@gmail.com>
2023-10-04 14:55:39 +0700

    Add category menu

--- src/main/java/com/poly/elnr/service/CategoryDetailService.java
+++ src/main/java/com/poly/elnr/service/CategoryDetailService.java
@@ -1,11 +1,11 @@
 package com.poly.elnr.service;
 
 import java.util.List;
+import java.util.Map;
 
-import com.poly.elnr.dto.CategoryDTO;
 
 public interface CategoryDetailService {
 
-	List<CategoryDTO> findCategoryAndCategoryDetail();
+	Map<String, List<String>> findCategoryAndCategoryDetail();
 	
 }
