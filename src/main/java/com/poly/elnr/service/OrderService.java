0588304fc6ef6e438d87b815365d81ca063d0613
PhanLeThanhPhuc <pltp2010@gmail.com>
2023-10-27 09:39:32 +0700

    user-info

--- src/main/java/com/poly/elnr/service/OrderService.java
+++ src/main/java/com/poly/elnr/service/OrderService.java
@@ -1,5 +1,13 @@
 package com.poly.elnr.service;
 
+import com.poly.elnr.entity.Order;
+
+import java.util.List;
+
 public interface OrderService {
+
+    List<Order> fillAllOrder();
+
+    Order fillOrderById(int id);
 
 }
--- src/main/java/com/poly/elnr/service/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/poly/elnr/service/serviceImpl/OrderServiceImpl.java
@@ -1,10 +1,28 @@
 package com.poly.elnr.service.serviceImpl;
 
+import com.poly.elnr.entity.Order;
+import com.poly.elnr.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.poly.elnr.service.OrderService;
 
+import java.util.List;
+
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    @Autowired
+    OrderRepository orderRepository;
+
+
+    @Override
+    public List<Order> fillAllOrder() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order fillOrderById(int id) {
+        return orderRepository.findById(id).get();
+    }
 }
