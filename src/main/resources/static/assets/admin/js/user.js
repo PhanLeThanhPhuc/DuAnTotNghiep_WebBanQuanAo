325ea8562408facdd488a2cdaef29f923de63de0
tinhdangtrung021003 <tinhdtps22991@fpt.edu.vngit config --global user.email tinhdtps22991@fpt.edu.vn>
2023-10-28 12:54:19 +0700

    tinh_autho_user

--- src/main/resources/static/assets/admin/js/user.js
+++ src/main/resources/static/assets/admin/js/user.js
@@ -3,7 +3,8 @@ app.controller("user-ctrl", function($scope, $http){
 		$http.get("/rest/users").then(resp => {
 			$scope.items = resp.data;
 			$scope.items.forEach(item => {
-				item.dateInsert = new Date(item.dateInsert)
+				item.date_insert = new Date(item.date_insert)
+				item.date_update = new Date(item.date_update)
 				
 			})
 		});
@@ -26,47 +27,51 @@ app.controller("user-ctrl", function($scope, $http){
 	$scope.create = function(){
 		var item = angular.copy($scope.form);
 		$http.post(`/rest/users`, item).then(resp => {
-			resp.data.dateInsert = new Date(resp.data.dateInsert)
+			resp.data.date_insert = new Date()
+			resp.data.date_update = new Date()
 			$scope.items.push(resp.data);
 			$scope.reset();
-			alert("Thêm mới sản phẩm thành công!");
+			alert("Thêm mới người dùng thành công!");
 		}).catch(error => {
-			alert("Lỗi thêm mới sản phẩm!");
+			alert("Lỗi thêm mới!");
 			console.log("Error", error);
 		});
 	}
 
 	$scope.update = function(){
 		var item = angular.copy($scope.form);
+		item.date_update = new Date();
 		$http.put(`/rest/users/${item.id}`, item).then(resp => {
 			var index = $scope.items.findIndex(p => p.id == item.id);
 			$scope.items[index] = item;
-			alert("Cập nhật sản phẩm thành công!");
+			alert("Cập nhật người dùng thành công!");
 		})
 		.catch(error => {
-			alert("Lỗi cập nhật sản phẩm!");
+			alert("Lỗi cập nhật!");
 			console.log("Error", error);
 		});
 	}
 
-	$scope.delete = function(item){
-		if(confirm("Bạn muốn xóa sản phẩm này?")){
-			$http.delete(`/rest/users/${item.id}`).then(resp => {
-				var index = $scope.items.findIndex(p => p.id == item.id);
-				$scope.items.splice(index, 1);
-				$scope.reset();
-				alert("Xóa sản phẩm thành công!");
-			}).catch(error => {
-				alert("Lỗi xóa sản phẩm!");
-				console.log("Error", error);
-			})
-		}
+	$scope.delete = function(){
+		var item = angular.copy($scope.form);
+		item.date_update = new Date();
+		item.status=false;
+		$http.put(`/rest/users/${item.id}`, item).then(resp => {
+			var index = $scope.items.findIndex(p => p.id == item.id);
+			$scope.items[index] = item;
+			alert("Cập nhật trạng thái thành công!");
+		})
+		.catch(error => {
+			alert("Lỗi cập nhật!");
+			console.log("Error", error);
+		});
+		
 	}
 	
 	$scope.imageChanged = function(files){
 		var data = new FormData();
 		data.append('file', files[0]);
-		$http.post('/rest/upload/images', data, {
+		$http.post('/rest/upload/image', data, {
 			transformRequest: angular.identity,
 			headers: {'Content-Type': undefined}
         }).then(resp => {
