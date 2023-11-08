09d0dc0d5ad93101a70d07b57cadf0f19c024e3e
loctran304 <minhloc3042002@gmail.com>
2023-11-08 12:10:16 +0700

    push save code

--- src/main/resources/static/assets/admin/js/voucher.js
+++ src/main/resources/static/assets/admin/js/voucher.js
@@ -1,96 +1,86 @@
-app.controller("voucher-ctrl", function($scope, $filter, $http) {
-	$scope.initialize = function() {
+app.controller("voucher-ctrl", function($scope,$filter, $http){
+	$scope.initialize = function(){
 		$http.get("/rest/products").then(resp => {
 			$scope.products = resp.data;
 		});
+		$http.get("/rest/voucher").then(resp => {
+			$scope.items = resp.data;
+			$scope.items.forEach(item => {
+				item.startDate = new Date(item.startDate)
+				item.endDate = new Date(item.endDate)
+			})
+		})
+		
 		$scope.reset();
 	}
-	$scope.searchText = {};
-	$scope.handleClick = function(value) {
-		$scope.searchText.rating = value;
-	};
-	$scope.reset = function() {
+	
+	$scope.reset = function(){
 		$scope.form = {
 		}
 	}
-	$scope.edit = function(item) {
+
+	$scope.edit = function(item){
 		$scope.form = angular.copy(item);
+		$(".nav-tabs a:eq(0)").tab("show");
 	}
-	$scope.delete = function(item) {
-		console.log(item.id)
-		if (confirm("Bạn muốn xóa đánh giá này?")) {
-			$http.delete(`/rest/review/${item.id}`).then(resp => {
-				var index = $scope.items.findIndex(p => p.id == item.id);
-				$scope.items.splice(index, 1);
-				$scope.reset();
-				alert("Xóa đánh giá thành công!");
-			}).catch(error => {
-				alert("Lỗi xóa đánh giá!");
-				console.log("Error", error);
-			})
-		}
-	}
-	$scope.initialize();
-	$scope.getFilteredData = function(rating) {
-		return $scope.items.filter(function(item) {
-			return item.rating == rating;
+
+	$scope.create = function(){
+		var item = angular.copy($scope.form);
+		$http.post(`/rest/voucher`, item).then(resp => {
+			resp.data.dateInsert = new Date(resp.data.dateInsert)
+			$scope.items.push(resp.data);
+			$scope.reset();
+			alert("Thêm mới sản phẩm thành công!");
+		}).catch(error => {
+			alert("Lỗi thêm mới sản phẩm!");
+			console.log("Error", error);
 		});
-	};
+	}
 
-	$scope.calculatePercentage = function(rating) {
-		var totalCount = $scope.items.length;
-		var matchingCount = 0;
-		for (var i = 0; i < totalCount; i++) {
-			if ($scope.items[i].rating == rating) {
-				matchingCount++;
-			}
-		}
-		var percentage = (matchingCount / totalCount) * 100;
-		percentages = percentage.toFixed(1);
-		if (percentages == 'NaN') {
-			percentages = 0;
-		}
-		return percentages;
-	};
+	$scope.update = function(){
+		var item = angular.copy($scope.form);
+		$http.put(`/rest/voucher/${item.id}`, item).then(resp => {
+			var index = $scope.items.findIndex(p => p.id == item.id);
+			$scope.items[index] = item;
+			alert("Cập nhật sản phẩm thành công!");
+		})
+		.catch(error => {
+			alert("Lỗi cập nhật sản phẩm!");
+			console.log("Error", error);
+		});
+	}
 
-	$scope.$watch('items', function() {
-		$scope.calculateAverage();
-	}, true);
-	$scope.calculateAverage = function() {
-		var sum = 0;
-		for (var i = 0; i < $scope.items.length; i++) {
-			sum += $scope.items[i].rating;
-		}
-		$scope.average = sum / $scope.items.length;
-		$scope.average = $scope.average.toFixed(1);
-		if ($scope.average == 'NaN') {
-			$scope.average = 0;
-		}
-	};
-	$scope.$watch('product.id', function(newValue) {
-		var id = 1;
-		if (newValue != 'all') {
-			$http.get("/rest/review/product/" + newValue).then(resp => {
-				$scope.items = resp.data;
-				$scope.items.forEach(item => {
-					item.dateReview = new Date(item.dateReview)
-				})
-			});
-			$scope.reset();
+
+	
+	$scope.updateStatus = function(size) {
+		var item = angular.copy(size);
+		if (item.status == false) {
+			item.status = true;
 		} else {
-			$http.get("/rest/review").then(resp => {
-				$scope.items = resp.data;
-				$scope.items.forEach(item => {
-					item.dateReview = new Date(item.dateReview)
-				})
-			});
-			$scope.reset();
+			item.status = false;
 		}
-	});
+		$http.put(`/rest/voucher/${item.id}`, item).then(resp => {
+			var index = $scope.items.findIndex(p => p.id == item.id);
+			$scope.items[index] = item;
+			
+			$scope.messege("Cập nhật trạng thái thành công");
+		})
+			.catch(error => {
+				alert("Lỗi cập nhật!");
+				console.log("Error", error);
+			});
+
+	}
+
+	$scope.initialize();
+
+	$scope.searchText = {};
 	$scope.items = [];
 	$scope.pager = {
 		page: 0,
 		size: 10,
+		sortColumn: '',
+		sortDirection: '',
 		get filteredItems() {
 			return $filter('filter')($scope.items, $scope.searchText);
 		},
@@ -102,6 +92,9 @@ app.controller("voucher-ctrl", function($scope, $filter, $http) {
 				this.first();
 			}
 			var filteredItems = this.filteredItems;
+			if ($scope.pager.sortColumn) {
+				filteredItems = $filter('orderBy')(filteredItems, $scope.pager.sortColumn, $scope.pager.sortDirection == 'desc');
+			}
 			var start = this.page * this.size;
 			return filteredItems.slice(start, start + this.size)
 		},
@@ -121,5 +114,64 @@ app.controller("voucher-ctrl", function($scope, $filter, $http) {
 			this.page--;
 		}
 	}
+	$scope.toggleSort = function(column) {
+		if ($scope.pager.sortColumn == column) {
+			$scope.pager.sortDirection = ($scope.pager.sortDirection == 'asc') ? 'desc' : 'asc';
+		} else {
+			$scope.pager.sortColumn = column;
+			$scope.pager.sortDirection = 'asc';
+		}
+	};
+	$scope.getSortIconClass = function(column) {
+		if ($scope.pager.sortColumn == column) {
+			return 'sort-icon ' + ($scope.pager.sortDirection == 'asc' ? 'asc' : 'desc');
+		}
+	};
+
+	$scope.isSortedBy = function(column) {
+		return $scope.pager.sortColumn == column;
+	};
+	
+	
+	$scope.messege = (mes) =>{
+	$.toast({
+	    text: mes, // Text that is to be shown in the toast
+	    heading: 'Thông báo', // Optional heading to be shown on the toast
+	    icon: 'success', // Type of toast icon
+	    showHideTransition: 'fade', // fade, slide or plain
+	    allowToastClose: true, // Boolean value true or false
+	    hideAfter: 2000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+	    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+	    position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+	    textAlign: 'left',  // Text alignment i.e. left, right or center
+	    loader: true,  // Whether to show loader or not. True by default
+	    loaderBg: '#9EC600',  // Background color of the toast loader
+	    beforeShow: function () {}, // will be triggered before the toast is shown
+	    afterShown: function () {}, // will be triggered after the toat has been shown
+	    beforeHide: function () {}, // will be triggered before the toast gets hidden
+	    afterHidden: function () {}  // will be triggered after the toast has been hidden
+	});
+	}
+	
+	
+	
+	
+	 $scope.generateRandomString = function() {
+      var length = Math.floor(Math.random() * 3) + 6; // Random length between 6 and 8
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var result = '';
+
+      for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+      }
+
+      $scope.form.voucher = result.toUpperCase();
+    };
+	
+	
+	
+	
+	
 }
 );
